e7e05beeaef0e466f15555ba546dc5a59c8f2732
cdi <cdi>
2025-02-26 17:08:39 +0100

    bug fix cors

---
+++ src/main/java/fr/ambuconnect/cors/CorsFilter.java
@@ -0,0 +1,51 @@
+package fr.ambuconnect.cors;
+
+import jakarta.annotation.Priority;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+import org.jboss.logging.Logger;
+import java.io.IOException;
+
+@Provider
+@Priority(1) // Priorité élevée pour s'exécuter avant d'autres filtres potentiels
+public class CorsFilter implements ContainerResponseFilter {
+    
+    private static final Logger LOG = Logger.getLogger(CorsFilter.class);
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        String origin = requestContext.getHeaderString("Origin");
+        LOG.info("Request from origin: " + origin);
+        
+        // Supprimer tous les en-têtes CORS existants pour éviter les duplications
+        responseContext.getHeaders().remove("Access-Control-Allow-Origin");
+        responseContext.getHeaders().remove("Access-Control-Allow-Credentials");
+        responseContext.getHeaders().remove("Access-Control-Allow-Headers");
+        responseContext.getHeaders().remove("Access-Control-Allow-Methods");
+        responseContext.getHeaders().remove("Access-Control-Max-Age");
+        
+        // Ajouter les en-têtes CORS appropriés
+        if (origin != null) {
+            responseContext.getHeaders().add("Access-Control-Allow-Origin", origin);
+            responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        } else {
+            // Fallback si l'origine est null (peu probable mais par sécurité)
+            responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        }
+        
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", 
+            "origin, content-type, accept, authorization, x-requested-with");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", 
+            "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH");
+        responseContext.getHeaders().add("Access-Control-Max-Age", "86400");
+        
+        // Traitement spécial pour OPTIONS (preflight)
+        if (requestContext.getMethod().equalsIgnoreCase("OPTIONS")) {
+            responseContext.setStatus(200);
+        }
+        
+        LOG.info("CORS headers set successfully");
+    }
+} 
