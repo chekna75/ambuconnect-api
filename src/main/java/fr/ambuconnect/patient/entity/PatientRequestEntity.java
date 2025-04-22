77f9ed58bb3887ccd37e898ccfef4499d9df6fb6
cdi <cdi>
2025-04-22 12:34:24 +0200

    Mise à jour des annotations des colonnes dans `PatientRequestEntity` pour spécifier les noms des colonnes dans la base de données. Cela améliore la clarté et la structure des entités, facilitant ainsi la gestion des données.

--- src/main/java/fr/ambuconnect/patient/entity/PatientRequestEntity.java
+++ src/main/java/fr/ambuconnect/patient/entity/PatientRequestEntity.java
@@ -32,36 +32,37 @@ public class PatientRequestEntity extends PanacheEntityBase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
     private UUID id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "patient_name")
     private String patientName;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "phone_number")
     private String phoneNumber;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "pickup_latitude")
     private Double pickupLatitude;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "pickup_longitude")
     private Double pickupLongitude;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "pickup_address")
     private String pickupAddress;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "destination_latitude")
     private Double destinationLatitude;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "destination_longitude")
     private Double destinationLongitude;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "destination_address")
     private String destinationAddress;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "requested_time")
     private LocalDateTime requestedTime;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "scheduled_time")
     private LocalDateTime scheduledTime;
 
     @ManyToOne
@@ -69,7 +70,7 @@ public class PatientRequestEntity extends PanacheEntityBase {
     private EntrepriseEntity assignedEntreprise;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = false, name = "status")
     private PatientRequestStatus status;
 
     @Column(name = "additional_notes", columnDefinition = "TEXT")
